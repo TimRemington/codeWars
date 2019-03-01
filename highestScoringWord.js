6b406f3800485d65336ad327a9f71d63955d093c
Tim Remington <Tim.Remington29@gmail.com>
2019-03-01 15:27:35 -0700

    added comments to scoring words to explain answer

--- highestScoringWord.js
+++ highestScoringWord.js
@@ -14,11 +14,13 @@ All letters will be lowercase and all inputs will be valid.
 
 function high(x){
   let splitarr = x.split(' ')
+  //Needed for scoring words
   let alphabet = {a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8, i:9, j:10, k:11, l:12, m:13, n:14, o:15, p:16, q:17, r:18, s:19, t:20, u:21, v:22, w:23, x:24, y:25, z:26}
   let result = ''
   let total = 0
   let highest = 0
 
+  // Nested for loop that goes through each word then scores each letter
   for (let i = 0; i < splitarr.length; i++) {
     total = 0
 
@@ -26,12 +28,13 @@ function high(x){
       total += alphabet[splitarr[i][j]]
     }
 
+    // If the total points are highest, reassigns score and saves word
     if (total > highest){
       highest = total
       result = splitarr[i]
     }
 
   }
-
+  //returns answer
   return result
 }
