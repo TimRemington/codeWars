f1f9f4ae8a81b4d21100b60c615d7bfdb8a47c86
Tim Remington <Tim.Remington29@gmail.com>
2019-03-07 16:53:13 -0700

    completed stringsAsNumbers.js and accounted for edge cases

--- stringsAsNumbers.js
+++ stringsAsNumbers.js
@@ -9,7 +9,30 @@ sumStrings('1','2') // => '3'
 A string representation of an integer will contain no characters besides the ten numerals "0" to "9"
 */
 
+const BigNumber = require('bignumber.js');
+
 function sumStrings(a,b) {
-  let total = Number(a) + Number(b)
-  return total.toString()
-}
+  console.log("a =====> ", typeof a)
+  console.log("b =====> ", b)
+
+  let numA, numB
+
+  if ( a === "") {
+    numA = 0
+  } else {
+    numA = new BigNumber(a)
+  }
+
+  if ( b === "") {
+    numB = 0
+  } else {
+    numB = new BigNumber(b)
+  }
+
+  let total = numB.plus(numA).toString()
+
+  if (total.length > 23) {
+    return total.slice(0,1) + total.slice(0, -4).slice(2)
+  } else {
+    return total
+  }
