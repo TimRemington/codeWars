400d2235de4800239bd32e4cac7db58a7bb1f9a3
Tim Remington <Tim.Remington29@gmail.com>
2019-03-07 18:07:39 -0700

    finished stringincrementer problem

--- stringIncrementer.js
+++ stringIncrementer.js
@@ -17,6 +17,36 @@ foo099 -> foo100
 
 Attention: If the number has leading zeros the amount of digits should be considered.*/
 
-function incrementString (strng) {
-  // return incrementedString
+function incrementString (str) {
+  if(str === "") {
+    return "1"
+  }
+
+  let regex = /[0-9]/g
+  let num = str.match(regex)
+
+  if (num === null) {
+    return str + "1"
+  }
+
+  let remover = num.length
+  let numbers = num.join('')
+  let zeros = ""
+
+  for (let i = 0; i < remover; i++){
+    zeros += "0"
+  }
+
+
+  let plusOne = (+numbers) + 1
+  let numString
+
+  if(plusOne.toString().length > zeros.length) {
+    numString = plusOne
+  } else {
+    numString = (zeros + plusOne).slice(-remover)
+  }
+
+  return str.substring(0, str.length - remover) + numString
+
 }
