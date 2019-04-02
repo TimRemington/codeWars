8d30162d67958a53a2391fa3598e66fd08537de4
Tim Remington <Tim.Remington29@gmail.com>
2019-04-02 13:35:59 -0600

    added numberSplitter.js

---
+++ numberSplitter.js
@@ -0,0 +1,17 @@
+// Doing a binary problem in codewars
+
+/*
+We define the operation of splitting a binary number n into two numbers a(n), b(n) as follows. Let 0 ≤ i1 < i2 < . . . < ik be the indices of the bits (with the least significant bit having index 0) in n that are 1. Then the indices of the bits of a(n) that are 1 are i1, i3, i5, . . . and the indices of the bits of b(n) that are 1 are i2, i4, i6, . . .
+
+For example, if n is 110110101 in binary then, again in binary, we have a = 010010001 and b = 100100100.
+
+Input
+Each test case consists of a single integer n between 1 and 2^31 − 1 in standard decimal (base 10) format.
+
+Output
+The output for each test case consists of a pair of integer, containing the integers a(n) and b(n). Both a(n) and b(n) should be in decimal format.
+*/
+
+function splitNumbers(n){
+  // your code here
+}
