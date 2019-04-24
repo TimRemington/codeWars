76ad094f8a07aba92ffb5cad4706433f3487c313
Tim Remington <Tim.Remington29@gmail.com>
2019-04-24 15:19:14 -0600

    finisehd orderedCounts.js

--- orderedCounts.js
+++ orderedCounts.js
@@ -9,6 +9,8 @@ orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]
 */
 
 var orderedCount = function (text) {
-  // Implement me!
-  return [];
+  let letters = text.split("")
+  let uniqs = letters.filter((letter, index) => letters.indexOf(letter) == index)
+  
+  return uniqs.map((letter) => [letter, text.split(letter).length - 1])
 }
