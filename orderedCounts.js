a3b140776b253feda9901c0a15165c5d7f4024f2
Tim Remington <Tim.Remington29@gmail.com>
2019-04-24 14:55:19 -0600

    created orderedCounts.js

---
+++ orderedCounts.js
@@ -0,0 +1,14 @@
+// Working on object creation problem in codewars
+
+/*
+Count the number of occurrences of each character and return it as a list of tuples in order of appearance.
+
+Example:
+
+orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+*/
+
+var orderedCount = function (text) {
+  // Implement me!
+  return [];
+}
