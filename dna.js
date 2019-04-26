77e70799bcfa3e20361ba04b9af7d8c2afdb5f7c
Tim Remington <Tim.Remington29@gmail.com>
2019-04-26 11:55:05 -0600

    finished dna.js

--- dna.js
+++ dna.js
@@ -15,5 +15,19 @@ DNAStrand ("GTAT") // return "CATA"
 */
 
 function DNAStrand(dna){
-  //your code here
+  let result = ""
+
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === "A") {
+      result += "T"
+    } else if (dna[i] === "T") {
+      result += "A"
+    } else if (dna[i] === "G") {
+      result += "C"
+    } else if (dna[i] === "C") {
+      result += "G"
+    }
+  }
+
+  return result
 }
