6985472a793f92e389846108e4b8b6e61e9576ef
Tim Remington <Tim.Remington29@gmail.com>
2019-05-13 12:34:10 -0600

    added consonants.js

---
+++ consonants.js
@@ -0,0 +1,23 @@
+// Working on string problem in codewars
+
+
+/*
+Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings.
+
+-- We shall assign the following values: a = 1, b = 2 ... z = 26.
+
+For example, for the word "zodiacs", solve("zodiacs") = 26
+-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+
+For the word "strength", solve("strength") = 57
+-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+Vowels are are a,e,i,o,u.
+
+For C: do not mutate input.
+
+More examples in test cases. Good luck!
+*/
+
+function solve(s) {
+  return 0;
+};
