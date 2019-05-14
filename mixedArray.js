96c8721b23bd4300ad105104bcbb5ec22840c742
Tim Remington <Tim.Remington29@gmail.com>
2019-05-14 11:26:44 -0600

    finished mixedArray.js

--- mixedArray.js
+++ mixedArray.js
@@ -7,5 +7,18 @@ Return your answer as a number.
 */
 
 function sumMix(x){
+  let result = 0
+  let temp = 0
+
+  for(let i = 0; i < x.length; i++) {
+    if(typeof x[i] === "string") {
+      temp = Number(x[i])
+      result += temp
+    } else {
+      result += x[i]
+    }
+  }
+
+  return result
 
 }
