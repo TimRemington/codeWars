03608aeac2d56608d40732cf14e73faf90b6331f
Tim Remington <Tim.Remington29@gmail.com>
2019-05-15 14:01:11 -0600

    created recursiveSum.js

---
+++ recursiveSum.js
@@ -0,0 +1,9 @@
+// working on basic recursion problem in codewars
+
+/*
+Write function sumR which returns the sum of values in a given list. Try no to cheat and provide recursive solution.
+*/
+
+function sumR(x) {
+  return 0;
+}
