065c60bf1b3e1b98a488be4a8605a72fdccdda2e
Tim Remington <Tim.Remington29@gmail.com>
2019-05-28 21:37:01 -0600

    added and finished space.js

---
+++ spaces.js
@@ -0,0 +1,10 @@
+// working on codewars
+
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+  let arr = x.split(' ')
+  return arr.join('')
+}
