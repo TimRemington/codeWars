045b1c4f0529df136541b3a96e2d953391f67a03
Tim Remington <Tim.Remington29@gmail.com>
2019-06-05 11:12:55 -0600

    finished reduceButGrow.js

--- reduceButGrow.js
+++ reduceButGrow.js
@@ -7,5 +7,9 @@ Given a non-empty array of integers, return the result of multiplying the values
 */
 
 function grow(x){
-
+  let result = 1
+  for(let i = 0; i < x.length; i++) {
+    result *= x[i]
+  }
+  return result
 }
