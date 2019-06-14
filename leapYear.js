628e6483ce25eb92ec202d13287a71a04c91fb97
Tim Remington <Tim.Remington29@gmail.com>
2019-06-14 11:20:28 -0600

    finished leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -12,5 +12,16 @@ Examples can be found in the test fixture.
 */
 
 function isLeapYear(year) {
-
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true
+      } else {
+        return false
+      }
+    } else {
+      return true
+    }
+  }
+  return false
 }
