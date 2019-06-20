520f5fd7236489902ed8ad7f42f7e2ed759a5ea9
Tim Remington <Tim.Remington29@gmail.com>
2019-06-19 18:26:13 -0600

    finished first run of alphabetPosition.js

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -15,5 +15,17 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 */
 
 function alphabetPosition(text) {
-  return text;
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"
+  let arr = text.split('')
+  let result = []
+  let temp
+
+  for (let i = 0; i < arr.length; i++) {
+    temp = arr[i].toLowerCase()
+    if (alphabet.search(temp) !== -1 && temp !== ".") {
+      result.push(alphabet.search(temp) + 1)
+    }
+  }
+
+  return result.join(' ')
 }
