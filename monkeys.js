6a282aa0e0c02073d6452ed965b6a1aefe6e11b2
Tim Remington <Tim.Remington29@gmail.com>
2019-06-25 19:19:18 -0600

    finished monkeys.js

--- monkeys.js
+++ monkeys.js
@@ -11,5 +11,11 @@ return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 */
 
 function monkeyCount(n) {
-// your code here
+  let result = []
+
+  for (let i = 1; i <= n; i++) {
+    result.push(i)
+  }
+
+  return result
 }
