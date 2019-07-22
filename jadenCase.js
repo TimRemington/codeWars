db9932f42131d8c9a2c717e9cda9c8eec2f80308
Tim Remington <Tim.Remington29@gmail.com>
2019-07-22 10:58:44 -0600

    finished jadenCase.js

--- jadenCase.js
+++ jadenCase.js
@@ -12,5 +12,7 @@ Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 */
 
 String.prototype.toJadenCase = function () {
-  //...
+  return this.split(" ").map(function(word){
+   return word.charAt(0).toUpperCase() + word.slice(1);
+ }).join(" ");
 };
