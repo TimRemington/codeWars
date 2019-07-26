9708f9bae321ac4d6ff790e67cee0f646c1c00e8
Tim Remington <Tim.Remington29@gmail.com>
2019-07-26 14:24:18 -0600

    Added multiples.js

---
+++ multiples.js
@@ -0,0 +1,16 @@
+// Working on daily codewars problem
+
+/*
+Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+Ex.
+
+multiples(3, 5)
+should return
+
+[5, 10, 15]
+*/
+
+function multiples(m, n){
+  // code here
+}
