9aef2a59196fc7ff8d18d2f304382d4e02ad070c
Tim Remington <Tim.Remington29@gmail.com>
2019-08-14 11:48:25 -0600

    quick codewars on stringArray.js

---
+++ stringArray.js
@@ -0,0 +1,17 @@
+// Quick codewars on a busy day
+
+/*
+
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+*/
+
+function stringToArray(string){
+
+	// code code code
+
+}
