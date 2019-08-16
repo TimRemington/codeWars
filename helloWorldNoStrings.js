9d4ebe65c2c89998a4fdcaaa12079edade990bfa
Tim Remington <Tim.Remington29@gmail.com>
2019-08-16 15:36:56 -0600

    finished helloWorldNoString.js

--- helloWorldNoStrings.js
+++ helloWorldNoStrings.js
@@ -12,5 +12,5 @@ Good luck and try to be as creative as possible!
 */
 
 const helloWorld = () => {
-  // feel free to change this into a function :)
+  return String.fromCharCode(72,101,108,108,111,44,32,87,111,114,108,100,33);
 };
