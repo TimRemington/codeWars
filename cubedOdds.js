8d2d4d2c5ba1d6a39944d07568c1562b34cc224f
Tim Remington <Tim.Remington29@gmail.com>
2019-09-02 11:19:28 -0600

    added cubedOdds.js

---
+++ cubedOdds.js
@@ -0,0 +1,13 @@
+// Working on codewars
+
+/*
+Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined/None/nil/NULL if any of the values aren't numbers.
+*/
+
+function cubeOdd(arr) {
+
+// insert code here >.<
+
+
+return 0;
+}
