bfba76a1d0ef6ae1f858a3203edeb00d48fd4059
Tim Remington <Tim.Remington29@gmail.com>
2019-09-02 11:21:01 -0600

    finished cubedOdd.js

--- cubedOdds.js
+++ cubedOdds.js
@@ -4,10 +4,9 @@
 Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined/None/nil/NULL if any of the values aren't numbers.
 */
 
-function cubeOdd(arr) {
-
-// insert code here >.<
-
-
-return 0;
+function cubeOdd(arr) 
+  const cubeOdd = (arr) => {
+    if (arr.filter(isNaN).length > 0) { return undefined };
+    return arr.reduce((total, num) => (num % 2) ? Math.pow(num, 3) + total : total, 0);
+  }
 }
