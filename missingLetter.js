9d495a45361b861aa3550d20a27062963300ab7d
Tim Remington <Tim.Remington29@gmail.com>
2019-09-04 12:51:04 -0600

    finished missingLetter.js

--- missingLetter.js
+++ missingLetter.js
@@ -21,5 +21,81 @@ I have also created other katas. Take a look if you enjoyed this kata!
 
 function findMissingLetter(array)
 {
-  return ' ';
+  const alpha = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+const alpham = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+let l = 0;
+let letter = "";
+
+if (array[0] === array[0].toUpperCase()) {
+  alpha.map((item, index) => {
+    if (item === array[0]) l = index;
+  });
+  while (array.includes(alpha[l])) {
+    l++;
+  }
+  letter = alpha[l];
+} else {
+  alpham.map((item, index) => {
+    if (item === array[0]) l = index;
+  });
+  while (array.includes(alpham[l])) {
+    l++;
+  }
+  letter = alpham[l];
+}
+return letter;
 }
